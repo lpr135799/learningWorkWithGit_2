d37b3e77388c27393992f9ac58134fa4e3da2c9d
Анапский Индустриальный Техникум <@aitanapa.org>
2024-11-15 11:39:12 +0300

    Adding pages in templates and adding events at page - secondCommit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
-
+headerMenu = ["Установка Flask", "Первое приложение", "Обратная связь"]
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Главная")
+    return render_template("index.html", title="Главная", menu=headerMenu)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="О сайте", menu=headerMenu)
 
