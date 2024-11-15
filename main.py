87b557931f8b816879345df6ee5d1d149e1f16eb
Анапский Индустриальный Техникум <@aitanapa.org>
2024-11-15 11:54:49 +0300

    Adding pages (base, contact, first_app, install) fill with use template and adding events- threeCommit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
-headerMenu = ["Установка Flask", "Первое приложение", "Обратная связь"]
+headerMenu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
 
 @app.route("/")
 def index():
@@ -11,3 +13,14 @@ def index():
 def about():
     return render_template('about.html', title="О сайте", menu=headerMenu)
 
+@app.route("/install-flask")
+def installFlask():
+    return render_template("install_flask.html", title="Установка", menu=headerMenu)
+
+@app.route("/first-app")
+def firstApp():
+    return render_template("first_app.html", title="Первое приложение", menu=headerMenu)
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Обратная связь", menu=headerMenu)
